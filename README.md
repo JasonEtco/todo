cbc941a71ec8ab2ade84166b6268ce1e2435ec0b
JasonEtco <jasonetco@gmail.com>
2017-09-28 20:25:02 -0400

    Add some configuration options

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # todo
 
-> a GitHub App built with [probot](https://github.com/probot/probot) that 
+> a GitHub App built with [probot](https://github.com/probot/probot) that creates new issues based on actionable comments in your code.
+
+## Usage
+
+Using `todo` should be really simple. Once you've installed it in your repository, simply push some code (to your default branch, a PR; doesn't matter). If the code you pushed includes the configured keyword (default is `@todo`), then the integration will create a new issue for you using the comment your wrote in your code!
+
+If I pushed this:
+
+```js
+/**
+ * Logs a message to the console
+ * @todo Change console.log to console.warn
+ */
+function logger() {
+  console.log('Hi!');
+}
+```
+
+`todo` would create a new issue:
+
+<IMG>
+
+## Configuring for your project
+
+There are a couple of configuration options in case you need to change the default behaviour. Note that the defaults are likely fine for most projects, so you might not need to change them.
+
+Add a file called `todo.yml` in the `.github` folder at the root of your project.
+
+### Available options
+
+| Name | Type | Description | Default |
+|------|------|-------------|---------|
+| autoAssign | `string`, `string[]` or `boolean` | Should `todo` automatically assign a user to the new issue? If `true`, it'll assign whoever pushed the code. If a string, it'll assign that user by username. You can also give it an array of usernames or `false` to not assign anyone. | `true` |
+| keyword | `string` | The keyword to use to generate issue titles | `@todo` |
+| blobLines | `number` or `false` | The number of lines of code to show, starting from the keyword. | 5 |
+| caseSensitive | `boolean` | Should the keyword be case sensitive? | false |
 
 ## Setup
 
--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-const {getContents, generateBody} = require('./helpers')
+const {defaultConfig, getContents, generateBody} = require('./helpers')
 
 module.exports = (robot) => {
   robot.on('push', async context => {
+    const cfg = await context.config('todo.yml', defaultConfig)
+
     const {head_commit} = context.payload
     const author = head_commit.author.username
 
+    // Put together list of new or changes files in this push
     const changedFiles = [...head_commit.added, ...head_commit.modified]
+
     const files = await changedFiles.reduce(async (prev, file) => {
+      // Get the file contents at the given SHA
       const contents = await getContents(context, file)
       return {...prev, [file]: contents}
     }, {})
 
+    // Get array of issue objects in the current repo
     const issues = await context.github.issues.getForRepo(context.issue())
 
     Object.keys(files).forEach(async file => {
       const contents = files[file]
-      // Get @todo titles
-      const titles = contents.match(/@todo\s(.*)/gi).map(title => title.replace('@todo ', ''))
+
+      // Get issue titles
+      const re = new RegExp(`${cfg.keyword}\\s(.*)`, cfg.caseSensitive ? 'g' : 'gi')
+      const titles = contents.match(re).map(title => title.replace(`${cfg.keyword} `, ''))
+      if (titles.length === 0) return
+
       titles.forEach(async title => {
         // Check if an issue with that title exists
         if (issues.data.some(issue => issue.title === title && issue.state === 'open')) return
 
-        // @todo Reopen existing but closed issues if the same todo is introduced
+        // :TODO: Reopen existing but closed issues if the same todo is introduced
 
-        const body = generateBody(title, file, contents, author, head_commit.id)
+        const body = generateBody(cfg, title, file, contents, author, head_commit.id)
 
         // Generate an issue object
-        // @todo Add labels
-        const issue = context.issue({ title, body, assignee: author })
+        // :TODO: Add labels
+
+        const issueObj = { title, body }
+        if (cfg.autoAssign === true) {
+          issueObj.assignee = author
+        } else if (typeof cfg.autoAssign === 'string') {
+          issueObj.assignee = cfg.autoAssign
+        } else if (Array.isArray(cfg.autoAssign)) {
+          issueObj.assignees = cfg.autoAssign
+        }
+
+        const issue = context.issue(issueObj)
 
         return context.github.issues.create(issue)
       })
