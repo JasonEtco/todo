2213c6c1616ffb4d07447a04f22ba0281db1b240
Jason Etcovitch <jasonetco@gmail.com>
2017-09-30 14:57:29 -0400

    Use all commits in a push, not just the head

--- index.js
+++ index.js
@@ -7,53 +7,62 @@ module.exports = (robot) => {
   robot.on('push', async context => {
     const cfg = await context.config('todo.yml', defaultConfig)
 
-    const {head_commit} = context.payload
-    const author = head_commit.author.username
-
-    // Put together list of new or changes files in this push
-    const changedFiles = [...head_commit.added, ...head_commit.modified]
-
-    const files = await changedFiles.reduce(async (prev, file) => {
-      // Get the file contents at the given SHA
-      const contents = await getContents(context, file)
-      return {...prev, [file]: contents}
-    }, {})
-
     // Get array of issue objects in the current repo
     const issues = await context.github.issues.getForRepo(context.issue())
 
-    Object.keys(files).forEach(async file => {
-      const contents = files[file]
+    const {head_commit, commits} = context.payload
+    const author = head_commit.author.username
 
-      // Get issue titles
-      const re = new RegExp(`${cfg.keyword}\\s(.*)`, cfg.caseSensitive ? 'g' : 'gi')
-      const titles = contents.match(re).map(title => title.replace(`${cfg.keyword} `, ''))
-      if (titles.length === 0) return
+    // Get the most up-to-date contents of each file
+    // by the commit it was most recently edited in.
+    const commitsByFiles = new Map()
+    for (let c = 0; c < commits.length; c++) {
+      const commit = commits[c]
+      const files = [...commit.added, ...commit.modified]
+      const mappedFiles = new Map()
 
-      titles.forEach(async title => {
-        // Check if an issue with that title exists
-        if (issues.data.some(issue => issue.title === title && issue.state === 'open')) return
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i]
+        if (commits[i + 1] && commits[i + 1].modified.indexOf(file) !== -1) return
+        const contents = await getContents(context, commit.id, file)
+        mappedFiles.set(file, contents)
+      }
 
-        // :TODO: Reopen existing but closed issues if the same todo is introduced
+      commitsByFiles.set(commit.id, mappedFiles)
+    }
 
-        const pr = await commitIsInPR(context, head_commit.id)
-        const body = generateBody(context, cfg, title, file, contents, author, head_commit.id, pr)
+    commitsByFiles.forEach(async (files, sha) => {
+      files.forEach(async (contents, file) => {
+        // Get issue titles
+        const re = new RegExp(`${cfg.keyword}\\s(.*)`, cfg.caseSensitive ? 'g' : 'gi')
+        const titles = contents.match(re).map(title => title.replace(`${cfg.keyword} `, ''))
+        if (titles.length === 0) return
 
-        // Generate an issue object
-        // :TODO: Add labels
+        titles.forEach(async title => {
+          // Check if an issue with that title exists
+          if (issues.data.some(issue => issue.title === title && issue.state === 'open')) return
 
-        const issueObj = { title, body }
-        if (cfg.autoAssign === true) {
-          issueObj.assignee = author
-        } else if (typeof cfg.autoAssign === 'string') {
-          issueObj.assignee = cfg.autoAssign
-        } else if (Array.isArray(cfg.autoAssign)) {
-          issueObj.assignees = cfg.autoAssign
-        }
+          // :TODO: Reopen existing but closed issues if the same todo is introduced
 
-        const issue = context.issue(issueObj)
+          const pr = await commitIsInPR(context, sha)
+          const body = generateBody(context, cfg, title, file, contents, author, sha, pr)
 
-        return context.github.issues.create(issue)
+          // Generate an issue object
+          // :TODO: Add labels
+
+          const issueObj = { title, body }
+          if (cfg.autoAssign === true) {
+            issueObj.assignee = author
+          } else if (typeof cfg.autoAssign === 'string') {
+            issueObj.assignee = cfg.autoAssign
+          } else if (Array.isArray(cfg.autoAssign)) {
+            issueObj.assignees = cfg.autoAssign
+          }
+
+          const issue = context.issue(issueObj)
+
+          return context.github.issues.create(issue)
+        })
       })
     })
   })
