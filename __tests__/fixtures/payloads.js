5fa7328a8891e9c008b07bed507c873a8123d523
JasonEtco <jasonetco@gmail.com>
2017-09-30 17:52:01 -0400

    Add some index tests

--- __tests__/fixtures/payloads.js
+++ __tests__/fixtures/payloads.js
@@ -5,7 +5,6 @@ exports.basic = {
     commits: [
       {
         id: 'f7d286aa6381bbb5045288496403d9427b0746e2',
-        author: { username: 'JasonEtco' },
         added: [],
         removed: [],
         modified: ['index.js']
@@ -24,9 +23,48 @@ exports.basic = {
         login: 'JasonEtco'
       }
     },
-    pusher: {
-      name: 'JasonEtco',
-      email: 'jasonetco@gmail.com'
+    installation: {
+      id: 55850
+    }
+  }
+}
+
+exports.complex = {
+  event: 'push',
+  payload: {
+    ref: 'refs/heads/master',
+    commits: [
+      {
+        id: 'f7d286aa6381bbb5045288496403d9427b0746e2',
+        added: [],
+        removed: [],
+        modified: ['index.js']
+      },
+      {
+        id: 'q7d286aa6381bbb5045288496403d9427b0746e2',
+        added: ['another.js'],
+        removed: [],
+        modified: []
+      },
+      {
+        id: 's7d286aa6381bbb5045288496403d9427b0746e2',
+        added: ['index.js'],
+        removed: [],
+        modified: []
+      }
+    ],
+    head_commit: {
+      id: 'f7d286aa6381bbb5045288496403d9427b0746e2',
+      author: { username: 'JasonEtco' }
+    },
+    repository: {
+      id: 98814489,
+      name: 'test',
+      full_name: 'JasonEtco/test',
+      owner: {
+        name: 'JasonEtco',
+        login: 'JasonEtco'
+      }
     },
     installation: {
       id: 55850
--- __tests__/index.js
+++ __tests__/index.js
@@ -12,11 +12,22 @@ describe('todo', () => {
 
     github = {
       issues: {
-        getForRepo: jest.fn()
+        getForRepo: jest.fn().mockReturnValue(Promise.resolve({ data: [] })),
+        create: jest.fn()
       },
       repos: {
         // Response for getting content from '.github/todo.yml'
-        getContent: jest.fn().mockReturnValue(Promise.resolve({ data: { content: Buffer.from('keyword: "@todo"') } }))
+        getContent: jest.fn((obj) => {
+          const ret = (str) => Promise.resolve({ data: { content: Buffer.from(str) } })
+          if (obj.path.includes('todo.yml')) {
+            return ret('keyword: "@todo"')
+          } else if (obj.path === 'index.js') {
+            return ret('\n\n@todo Jason!\nsdfasd\nsdfas\ndsfsa\n\n\nsdfsdaf')
+          }
+        })
+      },
+      pullRequests: {
+        getAll: jest.fn().mockReturnValue(Promise.resolve({ data: [{ head: { ref: 'master' }, number: 10 }] }))
       }
     }
     // Passes the mocked out GitHub API into out robot instance
@@ -24,11 +35,19 @@ describe('todo', () => {
   })
 
   describe('does a thing', () => {
-    it('performs an action', async () => {
-      // Simulates delivery of a payload
+    it('requests issues for the repo', async () => {
       await robot.receive(payloads.basic)
-      // This test would pass if in your main code you called `context.github.issues.createComment`
       expect(github.issues.getForRepo.mock.calls.length).toBe(1)
+    })
+
+    it('requests issues for the repo', async () => {
+      await robot.receive(payloads.basic)
+      expect(github.issues.getForRepo.mock.calls.length).toBe(1)
+    })
+
+    it('creates an issue', async () => {
+      await robot.receive(payloads.basic)
+      expect(github.issues.create.mock.calls.length).toBe(1)
     })
   })
 })
