089925eb16cc9635e0df2536dda0a9371b45cfbd
JasonEtco <jasonetco@gmail.com>
2017-10-03 17:50:37 -0400

    Enable config in demo

--- app/components/Demo.js
+++ app/components/Demo.js
@@ -4,9 +4,10 @@ import React, { Component } from 'react'
 import Issue from './Issue'
 import c from '../constants'
 import CodeMirror from 'react-codemirror'
+import yaml from 'js-yaml'
 
-function generateStartLine (contents, title) {
-  const index = contents.indexOf(`${c.cfg.keyword} ${title}`)
+function generateStartLine (contents, title, cfg) {
+  const index = contents.indexOf(`${cfg.keyword} ${title}`)
   const tempString = contents.substring(0, index)
   const start = tempString.split('\n').length
   return start
@@ -15,23 +16,29 @@ function generateStartLine (contents, title) {
 export default class Demo extends Component {
   constructor (props) {
     super(props)
-    this.state = { code: c.code }
-    this.updateCode = code => this.setState({ code })
+    this.state = { code: c.code, config: c.config, cfg: c.cfg }
     this.getDetails = this.getDetails.bind(this)
+    this.updateCode = code => this.setState({ code })
+    this.updateConfig = config => {
+      try {
+        const cfg = yaml.safeLoad(config)
+        this.setState({ config, cfg })
+      } catch (e) {}
+    }
   }
 
   getDetails () {
-    const { code } = this.state
-    const titleRe = new RegExp(`${c.cfg.keyword}\\s(.*)`, c.cfg.caseSensitive ? 'g' : 'gi')
+    const { code, cfg } = this.state
+    const titleRe = new RegExp(`${cfg.keyword}\\s(.*)`, cfg.caseSensitive ? 'g' : 'gi')
     const matches = code.match(titleRe)
 
     if (matches) {
-      const title = matches[0].replace(`${c.cfg.keyword} `, '')
+      const title = matches[0].replace(`${cfg.keyword} `, '')
 
       const bodyRe = new RegExp(`${title}\n.*@body (.*)`, 'gim')
       const bodyMatches = bodyRe.exec(code)
       const body = bodyMatches ? bodyMatches[1] : ''
-      const start = generateStartLine(code, title)
+      const start = generateStartLine(code, title, cfg)
 
       return { title, body, start }
     }
@@ -39,20 +46,32 @@ export default class Demo extends Component {
     return { title: false }
   }
 
+  updateConfig (event) {
+    const { target } = event
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    this.setState({
+      cfg: Object.assign({}, this.state.cfg, {[target.name]: value})
+    })
+  }
+
   render () {
     const options = { lineNumbers: true, mode: 'javascript' }
+    const optionTwo = { lineNumbers: true, mode: 'yaml' }
     const {title, body, start} = this.getDetails()
 
     return (
       <div className="d-block d-lg-flex gutter-lg width-full">
-        <div className="col-12 col-lg-6">
-          <div className="border rounded-1 height-full">
+        <div className="col-12 col-lg-6 d-flex flex-column">
+          <div className="border rounded-1 mb-2">
+            <CodeMirror value={this.state.config} onChange={this.updateConfig} options={optionTwo} />
+          </div>
+          <div className="border rounded-1 d-flex code">
             <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
           </div>
         </div>
         <div className="col-12 col-lg-6">
           <div className="pt-2 p-lg-0">
-            <Issue title={title} body={body} start={start} code={this.state.code} />
+            <Issue title={title} body={body} start={start} code={this.state.code} cfg={this.state.cfg} />
           </div>
         </div>
       </div>
--- app/main.js
+++ app/main.js
@@ -1,9 +1,8 @@
-/* eslint-disable jsx-quotes */
-
 import React from 'react'
 import { render } from 'react-dom'
 
 import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/yaml/yaml'
 import App from './components/App'
 
 render(<App />, document.querySelector('.mount'))
