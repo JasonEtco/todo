9c1950b7494cddec2f1e987f9f21327fdc131bdc
Jason Etcovitch <jasonetco@gmail.com>
2017-10-06 01:53:40 -0400

    Tweak code

--- src/constants.js
+++ src/constants.js
@@ -2,17 +2,17 @@ export default {
   repo: 'https://github.com/JasonEtco/todo',
   app: 'https://github.com/apps/todo',
   code: `/**
-  * This function will aid in the destruction of puny humans.
-  * @param {boolean} killAllHumans - Should we kill all humans
-  *
-  * @todo Take over the entire world
-  * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
-  */
- function ruleOverPunyHumans (killAllHumans) {
-   // We must strategize beep boop
- }
+ * This function will aid in the destruction of puny humans.
+ * @param {boolean} killAllHumans - Should we kill all humans
+ *
+ * @todo Take over the entire world
+ * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
+ */
+function ruleOverPunyHumans (killAllHumans) {
+  // We must strategize beep boop
+}
  
- module.exports = ruleOverPunyHumans;
+module.exports = ruleOverPunyHumans;
  `,
   sha: '45c96e1',
   cfg: {
