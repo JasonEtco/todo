75343bf417b77a1079320cb8880ec76e2ed680b9
Jason Etcovitch <jasonetco@gmail.com>
2017-10-08 23:37:03 -0400

    Make things work

--- handler.js
+++ handler.js
@@ -2,15 +2,16 @@
 const fs = require('fs')
 const todo = require('.')
 
+// eslint-disable-next-line
 require('file-loader?name=private-key.pem!./private-key.pem')
 const cert = fs.readFileSync('./private-key.pem', 'utf8')
 
 // Probot setup
-const createProbot = require('probot')
+const createProbot = require('./probot')
 const probot = createProbot({
+  cert,
   id: process.env.APP_ID,
   secret: process.env.WEBHOOK_SECRET,
-  cert: cert,
   port: 0
 })
 
