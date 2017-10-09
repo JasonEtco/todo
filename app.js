6c1b187ac39d826f500071aec7ee0938f95dfdcb
Matan Kushner <matchai@me.com>
2017-10-08 20:22:44 -0400

    Fix probot handler

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+// Webpack setup
+const fs = require('fs')
 const todo = require('.')
-const createProbot = require('probot')
-const cert = require('./private-key.pem')
+const cert = fs.readFileSync('private-key.pem', 'utf8')
 
-// Create probot instance
+// Probot setup
+const createProbot = require('probot')
 const probot = createProbot({
   id: process.env.APP_ID,
   secret: process.env.WEBHOOK_SECRET,
-  cert: cert
+  cert: cert,
+  port: 0
 })
 
-// Load probot application
+// Load Probot application
 probot.load(todo)
 
-module.exports.probotHandler = (event, context, callback) => {
-  // Check both cases because node's http server lowercases things
+// Lambda Handler
+module.exports.probotHandler = function (event, context, callback) {
+  // Determine incoming webhook event type
+  // Checking for different cases since node's http server is lowercasing everything
   const e = event.headers['x-github-event'] || event.headers['X-GitHub-Event']
 
   // Convert the payload to an Object if API Gateway stringifies it
   event.body = (typeof event.body === 'string') ? JSON.parse(event.body) : event.body
 
   try {
-    probot.receive(e, {
+    probot.receive({
       event: e,
       payload: event.body
     })
