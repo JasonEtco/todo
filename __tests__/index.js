70583acfdd8fbe0f14c6cb1d5cae2806adccdc76
JasonEtco <jasonetco@gmail.com>
2017-10-22 15:01:59 -0400

    Chunk out installation tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -246,29 +246,3 @@ describe('pr comments', () => {
     expect(github.pullRequests.createComment).toHaveBeenCalledTimes(1)
   })
 })
-
-describe('installation', () => {
-  let robot
-
-  beforeEach(() => {
-    robot = createRobot()
-    robot.auth = () => Promise.resolve({})
-    robot.log.info = jest.fn()
-    app(robot)
-  })
-
-  it('logs the proper message to the console', async () => {
-    await robot.receive(payloads.installCreatedOne)
-    expect(robot.log.info).toHaveBeenCalledWith('todo was just installed on JasonEtco/test.')
-  })
-
-  it('logs the proper message to the console w/ 2 repos', async () => {
-    await robot.receive(payloads.installCreatedTwo)
-    expect(robot.log.info).toHaveBeenCalledWith('todo was just installed on JasonEtco/test and JasonEtco/pizza.')
-  })
-
-  it('logs the proper message to the console w/ 3 repos', async () => {
-    await robot.receive(payloads.installCreatedThree)
-    expect(robot.log.info).toHaveBeenCalledWith('todo was just installed on JasonEtco/test, JasonEtco/pizza and JasonEtco/example.')
-  })
-})
