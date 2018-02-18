bbc4df1a898e329db7ca7844b8cc26798bf83a10
JasonEtco <jasonetco@gmail.com>
2018-02-17 20:20:35 -0500

    Write some more tests for the push handler

--- tests/pull-request-handler.test.js
+++ tests/pull-request-handler.test.js
@@ -14,19 +14,19 @@ describe('pr-comment-handler', () => {
   it('comments on a pull request', async () => {
     await robot.receive(event)
     expect(github.issues.createComment).toHaveBeenCalledTimes(1)
-    expect(github.issues.createComment.mock.calls[0][0]).toMatchSnapshot()
+    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
   })
 
   it('comments on a pull request and mentions the assigned user', async () => {
     github.repos.getContent.mockReturnValueOnce(loadConfig('autoAssignString'))
     await robot.receive(event)
-    expect(github.issues.createComment.mock.calls[0][0]).toMatchSnapshot()
+    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
   })
 
   it('comments on a pull request and mentions the assigned users', async () => {
     github.repos.getContent.mockReturnValueOnce(loadConfig('autoAssignArr'))
     await robot.receive(event)
-    expect(github.issues.createComment.mock.calls[0][0]).toMatchSnapshot()
+    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
   })
 
   it('does not create duplicate comments', async () => {
