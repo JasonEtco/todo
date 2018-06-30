07ac6d811f16b8f6286669b3e89af7c7821d3fab
Alexis Tyler <xo@wvvw.me>
2018-06-30 23:49:22 +0930

    fix test coverage

--- tests/push-handler.test.js
+++ tests/push-handler.test.js
@@ -78,10 +78,10 @@ describe('push-handler', () => {
   })
 
   it('ignores changes to the bin directory', async () => {
-    github.pullRequests.get.mockReturnValueOnce(loadDiff('bin'))
+    github.repos.getCommit.mockReturnValueOnce(loadDiff('bin'))
     github.repos.getContent.mockReturnValueOnce(loadConfig('excludeBin'))
     await robot.receive(event)
-    expect(github.issues.createComment).toHaveBeenCalledTimes(0)
+    expect(github.issues.create).toHaveBeenCalledTimes(0)
   })
 
   it('ignores pushes not to master', async () => {
