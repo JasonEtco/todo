6aa9120ebc7fbdd9e2d9b52ee7d13c9e8bd2598d
Jason Etcovitch <jasonetco@github.com>
2018-06-30 15:52:38 -0400

    Fix tests

--- src/utils/handler-setup.js
+++ src/utils/handler-setup.js
@@ -8,7 +8,7 @@ const generateLabel = require('./generate-label')
  */
 module.exports = async context => {
   const config = await context.config('config.yml')
-  const cfg = config && config.todo ? {...defaultConfig, ...config.todo} : defaultConfig
+  const cfg = config && config.todo ? Object.assign({}, defaultConfig, config.todo) : defaultConfig
   const labels = await generateLabel(context, cfg)
   const keywords = Array.isArray(cfg.keyword) ? cfg.keyword : [cfg.keyword]
   const reg = new RegExp(`^diff --git a\\/.+ b\\/(.+)[\\s\\S]+?^@{2}.+\\+(\\d+).+@{2}([\\s\\S]+?(^(\\+).*(${keywords.join('|')})(?:[:-\\s]+)?\\s(.*))[\\s\\S]+)`, 'gm')
