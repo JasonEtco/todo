462b4137d0c81ad6382f89a3e8f8c26c9ffbd598
Jason Etcovitch <jasonetco@github.com>
2018-11-18 17:32:45 -0500

    Refactor pr merge handler

--- src/pull-request-merged-handler.js
+++ src/pull-request-merged-handler.js
@@ -1,58 +1,52 @@
-const parseChunk = require('./utils/parse-chunk')
-const chunkDiff = require('./utils/chunk-diff')
 const checkForDuplicateIssue = require('./utils/check-for-duplicate-issue')
-const handlerSetup = require('./utils/handler-setup')
 const reopenClosed = require('./utils/reopen-closed')
-const { assignFlow, lineBreak } = require('./utils/helpers')
+const { assignFlow, lineBreak, truncate } = require('./utils/helpers')
 const { issueFromMerge } = require('./templates')
+const theLoop = require('./utils/the-loop')
 
 module.exports = async context => {
-  const [
-    { regex, config, labels },
-    chunks
-  ] = await Promise.all([
-    handlerSetup(context),
-    chunkDiff(context)
-  ])
-
-  const excludePattern = config.exclude
-
-  for (let i = 0; i < chunks.length; i++) {
-    const chunk = chunks[i]
-
-    let match
-    while ((match = regex.exec(chunk)) !== null) {
-      const parsed = parseChunk({ match, context, config })
-
-      if (parsed.filename === '.github/config.yml') {
-        context.log.debug('Skipping .github/config.yml')
-        continue
-      } else if (excludePattern && new RegExp(excludePattern).test(parsed.filename)) {
-        context.log.debug('Skipping ' + parsed.filename + ' as it matches the exclude pattern ' + excludePattern)
-        continue
-      }
-
-      // Prevent duplicates
-      const existingIssue = await checkForDuplicateIssue(context, parsed.title)
-      if (existingIssue) {
-        context.log(`Duplicate issue found with title [${parsed.title}]`)
-        await reopenClosed({ context, config, issue: existingIssue }, parsed)
-        continue
-      }
-
-      let body = issueFromMerge(context.repo({
-        sha: parsed.sha,
-        assignedToString: parsed.assignedToString,
-        range: parsed.range,
-        filename: parsed.filename,
-        keyword: parsed.keyword,
-        number: parsed.number,
-        body: parsed.body
-      }))
-
-      body = lineBreak(body)
-      context.log(`Creating issue [${parsed.title}] in [${context.repo().owner}/${context.repo().repo}]`)
-      await context.github.issues.create(context.repo({ title: parsed.title, body, labels, ...assignFlow(config, parsed.username) }))
+  return theLoop(context, async ({
+    title,
+    config,
+    filename,
+    range,
+    assignedToString,
+    keyword,
+    number,
+    bodyComment,
+    sha,
+    labels,
+    username
+  }) => {
+    // Prevent duplicates
+    const existingIssue = await checkForDuplicateIssue(context, title)
+    if (existingIssue) {
+      context.log(`Duplicate issue found with title [${title}]`)
+      return reopenClosed({ context, config, issue: existingIssue }, {
+        keyword,
+        title,
+        sha,
+        filename
+      })
     }
-  }
+
+    let body = issueFromMerge(context.repo({
+      sha,
+      assignedToString,
+      range,
+      filename,
+      keyword,
+      number,
+      body: bodyComment
+    }))
+
+    body = lineBreak(body)
+    context.log(`Creating issue [${title}] in [${context.repo().owner}/${context.repo().repo}]`)
+    return context.github.issues.create(context.repo({
+      title: truncate(title),
+      body,
+      labels,
+      ...assignFlow(config, username)
+    }))
+  })
 }
