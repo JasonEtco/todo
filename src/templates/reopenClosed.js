5236f6ecdd31a8a5082a8f553cd2a6a582d50e50
Jason Etcovitch <jasonetco@github.com>
2018-11-18 17:32:33 -0500

    Typo in template

--- src/templates/reopenClosed.js
+++ src/templates/reopenClosed.js
@@ -1,4 +1,4 @@
-module.exports = `This issue has been reopened because the **\`{{ keyword }}\`** comment still exists in [**{{ filename }}**](https://github.com/{{ owner }}/{{ repo}}/blob/{{ sha }}/{{ filename }}), as of {{ sha }}.
+module.exports = `This issue has been reopened because the **\`{{ keyword }}\`** comment still exists in [**{{ filename }}**](https://github.com/{{ owner }}/{{ repo }}/blob/{{ sha }}/{{ filename }}), as of {{ sha }}.
 
 ---
 
