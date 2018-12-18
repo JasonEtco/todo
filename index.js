4172ba450fb417f3a118d44c811e22b0514de889
Jason Etcovitch <jasonetco@github.com>
2018-12-18 13:52:43 -0500

    Middleware to log when new events are passed

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const pullRequestMergedHandler = require('./lib/pull-request-merged-handler')
 const pushHandler = require('./lib/push-handler')
 const issueRenameHandler = require('./lib/issue-rename-handler')
 
+const handle = handler => {
+  return async context => {
+    context.log(context.repo(), 'Receive event')
+    return handler(context)
+  }
+}
+
 module.exports = app => {
   // PR handler (comments on pull requests)
-  app.on(['pull_request.opened', 'pull_request.synchronize'], pullRequestHandler)
+  app.on(['pull_request.opened', 'pull_request.synchronize'], handle(pullRequestHandler))
 
   // Merge handler (opens new issues)
-  app.on('pull_request.closed', pullRequestMergedHandler)
+  app.on('pull_request.closed', handle(pullRequestMergedHandler))
 
   // Push handler (opens new issues)
-  app.on('push', pushHandler)
+  app.on('push', handle(pushHandler))
 
   // Prevent tampering with the issue title
-  app.on('issues.edited', issueRenameHandler)
+  app.on('issues.edited', handle(issueRenameHandler))
 }
