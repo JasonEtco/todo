200c672ca4c4d77a0ea95958d44361c53c5f20d0
Jason Etcovitch <jasonetco@github.com>
2018-12-29 22:59:54 -0500

    Add tests

--- tests/push-handler.test.js
+++ tests/push-handler.test.js
@@ -149,4 +149,28 @@ describe('push-handler', () => {
     await app.receive(event)
     expect(github.issues.create.mock.calls[0]).toMatchSnapshot()
   })
+
+  it('closes an issue on a removed TODO', async () => {
+    github.search.issues.mockReturnValueOnce(Promise.resolve({
+      data: { total_count: 1, items: [{ title: 'I am an example title', number: 1 }] }
+    }))
+    github.repos.getCommit.mockReturnValueOnce(loadDiff('remove-todo'))
+    await app.receive(event)
+    expect(github.issues.edit).toHaveBeenCalled()
+    expect(github.issues.edit).toHaveBeenCalledWith({
+      number: 1,
+      owner: 'JasonEtco',
+      repo: 'tests',
+      state: 'closed'
+    })
+  })
+
+  it('does nothing on a non-existant issue on a removed TODO', async () => {
+    github.search.issues.mockReturnValueOnce(Promise.resolve({
+      data: { total_count: 0, items: [] }
+    }))
+    github.repos.getCommit.mockReturnValueOnce(loadDiff('remove-todo'))
+    await app.receive(event)
+    expect(github.issues.edit).not.toHaveBeenCalled()
+  })
 })
