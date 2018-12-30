3b6436f15f95c331e39c8dd331926f334fcf5d3a
Jason Etcovitch <jasonetco@github.com>
2018-12-29 23:01:16 -0500

    Add pull-request-merged-handler tests

--- tests/pull-request-merge-handler.test.js
+++ tests/pull-request-merge-handler.test.js
@@ -114,4 +114,28 @@ describe('pull-request-merged-handler', () => {
     expect(github.issues.createComment).not.toHaveBeenCalled()
     expect(github.issues.create).not.toHaveBeenCalled()
   })
+
+  it('closes an issue on a removed TODO', async () => {
+    github.search.issues.mockReturnValueOnce(Promise.resolve({
+      data: { total_count: 1, items: [{ title: 'I am an example title', number: 1 }] }
+    }))
+    github.pullRequests.get.mockReturnValueOnce(loadDiff('remove-todo'))
+    await app.receive(event)
+    expect(github.issues.edit).toHaveBeenCalled()
+    expect(github.issues.edit).toHaveBeenCalledWith({
+      number: 1,
+      owner: 'JasonEtco',
+      repo: 'tests',
+      state: 'closed'
+    })
+  })
+
+  it('does nothing on a non-existant issue on a removed TODO', async () => {
+    github.search.issues.mockReturnValueOnce(Promise.resolve({
+      data: { total_count: 0, items: [] }
+    }))
+    github.pullRequests.get.mockReturnValueOnce(loadDiff('remove-todo'))
+    await app.receive(event)
+    expect(github.issues.edit).not.toHaveBeenCalled()
+  })
 })
