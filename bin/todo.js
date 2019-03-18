49cd8759075a4bb984931b877dccbd8dc35bff62
Jason Etcovitch <jasonetco@github.com>
2019-03-17 23:48:06 -0400

    Use Probot's GitHubAPI

--- bin/todo.js
+++ bin/todo.js
@@ -2,13 +2,13 @@
 
 const program = require('commander')
 const chalk = require('chalk')
-const GitHubAPI = require('@octokit/rest')
+const { GitHubAPI } = require('probot/lib/github')
 const pushHandler = require('../lib/push-handler')
 const pullRequestMergedHandler = require('../lib/pull-request-merged-handler')
 const fs = require('fs')
 const path = require('path')
 
-const octokit = new GitHubAPI()
+const github = new GitHubAPI()
 
 program
   .option('-o, --owner <owner>', 'owner')
