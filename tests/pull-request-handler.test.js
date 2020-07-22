a353158a170aba7264cc8c989d510721c675f779
Gregor Martynus <gregor@martynus.net>
2020-07-22 00:36:31 -0700

    WIP: rewrite pull-request-handler test to use nock (1/9)

--- tests/pull-request-handler.test.js
+++ tests/pull-request-handler.test.js
@@ -1,20 +1,52 @@
+const nock = require('nock')
+const { Probot } = require('probot')
+
 const { gimmeApp, loadConfig, loadDiff } = require('./helpers')
 const pullRequestOpened = require('./fixtures/payloads/pull_request.opened.json')
+const plugin = require('..')
 
 describe('pull-request-handler', () => {
-  let app, github
+  let probot
   const event = { name: 'pull_request', payload: pullRequestOpened }
 
   beforeEach(() => {
-    const gimme = gimmeApp()
-    app = gimme.app
-    github = gimme.github
+    probot = new Probot({
+      id: 1,
+      githubToken: 'secret',
+      throttleOptions: {
+        enabled: false
+      }
+    })
+    probot.load(plugin)
   })
 
-  it('comments on a pull request', async () => {
-    await app.receive(event)
-    expect(github.issues.createComment).toHaveBeenCalledTimes(1)
-    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
+  it.only('comments on a pull request', async () => {
+    const mock = nock('https://api.github.com')
+    
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('basic')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(404, {})
+      .get('/repos/JasonEtco/.github/contents/.github/config.yml')
+      .reply(404, {})
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('comments on a pull request and mentions the assigned user', async () => {
