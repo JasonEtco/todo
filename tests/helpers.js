435523f212ef489c32d4a8d816534ef4cbb0ec29
Gregor Martynus <gregor@martynus.net>
2020-08-12 16:32:07 -0700

    WIP rewrite pull-request-handler.test.js using nock

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,71 +1,11 @@
 const fs = require('fs')
 const path = require('path')
-const { Application } = require('probot')
-const plugin = require('..')
 
-const loadDiff = exports.loadDiff = filename => {
+exports.loadDiff = filename => {
   return Promise.resolve({
     data: fs.readFileSync(path.join(__dirname, 'fixtures', 'diffs', filename + '.txt'), 'utf8'),
     headers: { 'content-length': 1 }
   })
 }
 
-exports.loadConfig = filename => {
-  return Promise.resolve({
-    data: {
-      content: fs.readFileSync(path.join(__dirname, 'fixtures', 'configs', filename + '.yml'), 'base64')
-    }
-  })
-}
-
-exports.gimmeApp = () => {
-  const logger = {
-    trace: jest.fn(),
-    debug: jest.fn(),
-    info: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn(),
-    fatal: jest.fn()
-  }
-
-  const app = new Application({
-    logger,
-    app: {
-      getInstallationAccessToken: jest.fn().mockResolvedValue('test'),
-      getSignedJsonWebToken: jest.fn().mockReturnValue('test')
-    }
-  })
-  app.load(plugin)
-
-  const github = {
-    issues: {
-      create: jest.fn(data => Promise.resolve({ data })).mockName('issues.create'),
-      createLabel: jest.fn().mockName('issues.createLabel'),
-      update: jest.fn().mockName('issues.update'),
-      createComment: jest.fn().mockName('issues.createComment'),
-      listComments: jest.fn(() => Promise.resolve({ data: [] })).mockName('issues.listComments')
-    },
-    search: {
-      issuesAndPullRequests: jest.fn(() => Promise.resolve({ data: { total_count: 0, items: [] } })).mockName('search.issuesAndPullRequests')
-    },
-    git: {
-      getCommit: jest.fn(() => Promise.resolve({ data: { parents: [1] } })).mockName('git.getCommit')
-    },
-    repos: {
-      // Response for getting content from '.github/todo.yml'
-      getContents: jest.fn(() => {
-        throw { status: 404 } // eslint-disable-line
-      }).mockName('repos.getContents'),
-      getCommit: jest.fn(() => loadDiff('basic')).mockName('repos.getCommit')
-    },
-    pulls: {
-      get: jest.fn(() => loadDiff('basic')).mockName('pulls.get')
-    },
-    hook: {
-      before: jest.fn()
-    }
-  }
-  // Passes the mocked out GitHub API into out app instance
-  app.auth = () => Promise.resolve(github)
-  return { app, github }
-}
+exports.loadConfig = filename => fs.readFileSync(path.join(__dirname, 'fixtures', 'configs', filename + '.yml'))
--- tests/pull-request-handler.test.js
+++ tests/pull-request-handler.test.js
@@ -1,7 +1,7 @@
 const nock = require('nock')
 const { Probot } = require('probot')
 
-const { gimmeApp, loadConfig, loadDiff } = require('./helpers')
+const { loadConfig, loadDiff } = require('./helpers')
 const pullRequestOpened = require('./fixtures/payloads/pull_request.opened.json')
 const plugin = require('..')
 
@@ -20,9 +20,9 @@ describe('pull-request-handler', () => {
     probot.load(plugin)
   })
 
-  it.only('comments on a pull request', async () => {
+  it('comments on a pull request', async () => {
     const mock = nock('https://api.github.com')
-    
+
       .get('/repos/JasonEtco/tests/issues/32/comments')
       .reply(200, [])
 
@@ -50,57 +50,216 @@ describe('pull-request-handler', () => {
   })
 
   it('comments on a pull request and mentions the assigned user', async () => {
-    github.repos.getContents.mockReturnValueOnce(loadConfig('autoAssignString'))
-    await app.receive(event)
-    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('basic')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(200, {
+        content: loadConfig('autoAssignString').toString('base64')
+      })
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('comments on a pull request and mentions the assigned users', async () => {
-    github.repos.getContents.mockReturnValueOnce(loadConfig('autoAssignArr'))
-    await app.receive(event)
-    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('basic')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(200, {
+        content: loadConfig('autoAssignArr').toString('base64')
+      })
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('does not create duplicate comments', async () => {
-    github.issues.listComments.mockReturnValueOnce(Promise.resolve({
-      data: [{
-        body: '## I am an example title'
-      }]
-    }))
+    const mock = nock('https://api.github.com')
 
-    await app.receive(event)
-    expect(github.issues.createComment).not.toHaveBeenCalled()
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [{
+        body: '## I am an example title'
+      }])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('basic')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(200, {
+        content: loadConfig('autoAssignArr').toString('base64')
+      })
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('creates many (5) comments', async () => {
-    github.pulls.get.mockReturnValue(loadDiff('many'))
-    await app.receive(event)
-    expect(github.issues.createComment).toHaveBeenCalledTimes(5)
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('many')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(404, {})
+      .get('/repos/JasonEtco/.github/contents/.github/config.yml')
+      .reply(404, {})
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .times(5)
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('ignores changes to the config file', async () => {
-    github.pulls.get.mockReturnValue(loadDiff('config'))
-    await app.receive(event)
-    expect(github.issues.createComment).not.toHaveBeenCalled()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('config')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(404, {})
+      .get('/repos/JasonEtco/.github/contents/.github/config.yml')
+      .reply(404, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('ignores changes to the bin directory', async () => {
-    github.pulls.get.mockReturnValue(loadDiff('bin'))
-    github.repos.getContents.mockReturnValueOnce(loadConfig('excludeBin'))
-    await app.receive(event)
-    expect(github.issues.createComment).not.toHaveBeenCalled()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('bin')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(200, {
+        content: loadConfig('excludeBin').toString('base64')
+      })
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('works with a string as the keyword config', async () => {
-    github.repos.getContents.mockReturnValueOnce(loadConfig('keywordsString'))
-    github.pulls.get.mockReturnValue(loadDiff('custom-keyword'))
-    await app.receive(event)
-    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('custom-keyword')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(200, {
+        content: loadConfig('keywordsString').toString('base64')
+      })
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 
   it('creates a comment with a body line', async () => {
-    github.pulls.get.mockReturnValue(loadDiff('body'))
-    await app.receive(event)
-    expect(github.issues.createComment.mock.calls[0]).toMatchSnapshot()
+    const mock = nock('https://api.github.com')
+
+      .get('/repos/JasonEtco/tests/issues/32/comments')
+      .reply(200, [])
+
+      .head('/repos/JasonEtco/tests/pulls/32')
+      .reply(200)
+
+      .get('/repos/JasonEtco/tests/pulls/32')
+      .matchHeader('accept', 'application/vnd.github.diff')
+      .reply(200, (await loadDiff('body')).data)
+
+      .get('/repos/JasonEtco/tests/contents/.github/config.yml')
+      .reply(404, {})
+      .get('/repos/JasonEtco/.github/contents/.github/config.yml')
+      .reply(404, {})
+
+      .post('/repos/JasonEtco/tests/issues/32/comments', parameters => {
+        expect(parameters).toMatchSnapshot()
+
+        return true
+      })
+      .reply(201, {})
+
+    await probot.receive(event)
+    expect(mock.activeMocks()).toStrictEqual([])
   })
 })
