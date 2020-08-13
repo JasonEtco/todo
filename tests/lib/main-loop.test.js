1602cb5270388500cfa0ad41843eacc9a94c6c39
Gregor Martynus <gregor@martynus.net>
2020-08-12 20:58:52 -0700

    WIP final tweaks

--- tests/lib/main-loop.test.js
+++ tests/lib/main-loop.test.js
@@ -1,7 +1,7 @@
 const mainLoop = require('../../lib/utils/main-loop')
 const { loadDiff } = require('../helpers')
 
-describe.skip('main-loop', () => {
+describe('main-loop', () => {
   let context, handler
 
   beforeEach(() => {
@@ -20,7 +20,12 @@ describe.skip('main-loop', () => {
           createLabel: jest.fn()
         },
         repos: {
-          getCommit: jest.fn(() => loadDiff('basic')).mockName('repos.getCommit')
+          getCommit: jest.fn(() => {
+            return {
+              data: loadDiff('basic'),
+              headers: { 'content-length': 1 }
+            }
+          }).mockName('repos.getCommit')
         }
       }
     }
@@ -49,13 +54,23 @@ describe.skip('main-loop', () => {
   })
 
   it('does nothing if a title is only whitespace', async () => {
-    context.github.repos.getCommit.mockReturnValue(Promise.resolve(loadDiff('title-with-whitespace')))
+    context.github.repos.getCommit.mockResolvedValue({
+      data: loadDiff('title-with-whitespace'),
+      headers: {
+        'content-length': 1
+      }
+    })
     await mainLoop(context, handler)
     expect(handler).not.toHaveBeenCalled()
   })
 
   it('does nothing if a keyword is in the middle of a sentence', async () => {
-    context.github.repos.getCommit.mockReturnValue(Promise.resolve(loadDiff('middle-of-sentence')))
+    context.github.repos.getCommit.mockResolvedValue({
+      data: loadDiff('middle-of-sentence'),
+      headers: {
+        'content-length': 1
+      }
+    })
     await mainLoop(context, handler)
     expect(handler).not.toHaveBeenCalled()
   })
